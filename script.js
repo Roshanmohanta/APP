885283cba98e1ba54d0c2c991dd4fe9a2a3a217b
Roshanmohanta <roshanmohanta2004@gmail.com>
2024-10-07 09:23:49 +0530

    front_end

---
+++ script.js
@@ -0,0 +1,37 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+});
+
+
+// homepage starts
+
+
+let profile = document.querySelector('.header1 .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    profile.classList.remove('active');
+}
---
+++ scripts.js
@@ -0,0 +1,84 @@
+
+
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
+
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+
+
+let searchForm = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    profile.classList.remove('active');
+}
+
+
+
+let sidebar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    sidebar.classList.toggle('active');
+    body.classList.toggle('active');
+    
+}
+
+
+
+
+document.querySelector('.side-bar .close-side-bar').onclick = () =>{
+    sidebar.classList.remove('active');
+    body.classList.remove('active');
+    
+    
+}
+
+
+
+
+window.onscroll = () =>{
+    profile.classList.remove('active');
+    searchForm.classList.remove('active');
+    if(window.innerWidth < 1200){
+        sidebar.classList.remove('active');
+        body.classList.remove('active');
+
+    }
+}
+
+
+
+
